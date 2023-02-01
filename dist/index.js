ac3f112e83ef3c2b739e38a426b219078bb97604
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2023-02-01 14:11:12 +0000

    Added nunjucks engine

--- dist/index.js
+++ dist/index.js
@@ -5,11 +5,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const nunjucks_1 = __importDefault(require("nunjucks"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const port = process.env.PORT;
+nunjucks_1.default.configure('views', {
+    autoescape: true,
+    express: app,
+});
 app.get('/', (req, res) => {
-    res.send('Express + TypeScript Server');
+    res.render('index.njk', { layout: 'layout.njk', message: 'I am update' });
 });
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
--- index.ts
+++ index.ts
@@ -1,13 +1,19 @@
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
+import nunjucks from 'nunjucks';
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT;
 
+nunjucks.configure('views', {
+  autoescape: true,
+  express: app,
+});
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('Express + TypeScript Server');
+  res.render('index.njk', { layout: 'layout.njk', message: 'I am update' });
 });
 
 app.listen(port, () => {
