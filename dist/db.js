1358787dfd0d6a25d3beaf6b3f8b3ec8546f9606
Harley Harris <harley.harris@digital.cabinet-office.gov.uk>
2023-02-06 14:34:54 +0000

    Added connection pool logic (db.ts) using postgres-node to connect to local postgres database.

---
+++ dist/db.js
@@ -0,0 +1,36 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const { Pool } = require('pg');
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const connectDb = () => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const pool = new Pool({
+            user: process.env.PGUSER,
+            host: process.env.PGHOST,
+            database: process.env.PGDATABASE,
+            password: process.env.PGPASSWORD,
+            port: process.env.PGPORT
+        });
+        yield pool.connect();
+        const res = yield pool.query('SELECT * FROM journal_entry');
+        console.log(res);
+        yield pool.end();
+    }
+    catch (error) {
+        console.log(error);
+    }
+});
+connectDb();
